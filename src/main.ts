dd05d9dc5d6e5b1ae774fdff4277cafd25c9752d
vimal2046 <vimalchandar2003@gmail.com>
2025-09-15 18:38:24 +0530

    feat: enable production mode and update analytics injection logic

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { inject } from '@vercel/analytics';
+import { enableProdMode, isDevMode } from '@angular/core';
  
-inject();
+if (!isDevMode()) {
+  enableProdMode();
+}
+
+// Inject analytics script before bootstrapping app
+inject({ mode: isDevMode() ? 'development' : 'production' });
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
